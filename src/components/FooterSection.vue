febdac8ffaf131710ae7ce45a2341cbf63917259
amissu89 <yonglimlee@gmail.com>
2024-11-29 14:03:11 +0900

    footer 번경 및 mainpage의 내용 수정

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,13 +1,6 @@
 <template lang="">
     <div class="container">
         <div class="row">
-            <div class="col">
-                <p>Lee yonglim's Archaiving</p>
-            </div>
-            
-            <div class="col"> 
-                <p>yonglimlee@gmail.com</p>
-            </div>
             <div class="col"> 
                 <p> &copy; Lee yonglim | All Rights Reserved </p>
             </div>
@@ -23,12 +16,12 @@ export default {
 
 <style scoped>
 .container{
-    border: solid 1px black;
     height: 5vh;
     margin-top:1vh;
+    text-align: right;
 }    
 
-.row{
+/* .row{
     padding: 1em 1em 1em 1em;
-}
+} */
 </style>
