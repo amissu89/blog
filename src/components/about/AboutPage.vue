de8d7dc29d88144e765e25107540575fdbed14b5
amissu89 <yonglimlee@gmail.com>
2025-02-06 12:54:13 +0900

    Update about page

--- src/components/about/AboutPage.vue
+++ src/components/about/AboutPage.vue
@@ -1,6 +1,31 @@
 <template lang="">
     <div>
-        about
+        <div class="row">
+            <h3>Education</h3>
+            <p>
+                한국기술교육대학교 Korea University of Technology and Education <br/>
+                2008.03 ~ 2013.02 <br/>
+                컴퓨터 공학부 학사 졸업 Bachelor's Degree in Computer Engineering
+            </p>
+        </div>
+
+        <div class="row">
+            <h3>Trainning & Experience</h3>
+            <ul>
+                <li>
+                    <b>University Appropriate Technology Project</b> <br/>
+                    2012.06 <br/>   
+                    GIS Development Using Google Maps <br/>
+                </li>
+
+                <li>
+                    <b>Dale Carnegie Training</b> <br/>
+                    2012.02 <br/>
+                    Leadership development and personal growth sessions focused on goal setting to achieve both short-term and long-term personal objectives. <br/>
+                </li>
+            </ul>
+
+        </div>
     </div>
 </template>
 <script>
