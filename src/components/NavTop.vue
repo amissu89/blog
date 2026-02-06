a37a1284b4df28e7b6b9c495dcb8185988e42813
yonglimlee@gmail.com <yonglimlee@gmail.com>
2026-02-06 15:15:07 +0900

    remove dark mode

--- src/components/NavTop.vue
+++ src/components/NavTop.vue
@@ -135,11 +135,6 @@ const signOut = async () => {
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='%233A3635' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
 }
 
-@media (prefers-color-scheme: dark) {
-  .navbar-toggler-icon {
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='%23F5F1ED' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
-  }
-}
 
 /* Collapse Menu */
 .collapse.navbar-collapse {
